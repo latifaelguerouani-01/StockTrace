0405cb3a8629bcf2c14f7b7872e93cab9176c727
latifaelguerouani-01 <latifaelguerouani@gmail.com>
2026-08-18 21:41:50 +0100

    Add files via upload

---
+++ src/main/java/com/stocktrace/stocktrace/StocktraceApplication.java
@@ -0,0 +1,12 @@
+package com.stocktrace.stocktrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktraceApplication.class, args);
+	}
+}
